53d7687096a3c0d88c1d0cb38757e57504dcf9be
v.la <v.la@live.cn>
2019-04-16 21:46:17 +0800

    test: add RedisBenchmark

---
+++ test/BenchmarkTest/RedisBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BloomFilter;
+using BloomFilter.Redis;
+
+namespace BenchmarkTest
+{
+    [ClrJob, CoreJob]
+    [MemoryDiagnoser]
+    public class RedisBenchmark
+    {
+        private Filter<string> filter;
+        private byte[] data;
+
+
+        [Params(64)]
+        public int DataSize;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var n = 1000000;
+            var errRate = 0.01;
+
+            data = Helper.GenerateBytes(DataSize);
+            filter = FilterRedisBuilder.Build<string>("localhost", "RedisBloomFilterTest", n, errRate,  HashMethod.Murmur3KirschMitzenmacher);
+            filter.Clear();
+        }
+
+        [Benchmark]
+        public void Add()
+        {
+            filter.Add(data);
+        }
+    }
+}
