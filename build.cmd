f0206ae40bb10a1f764eec4d55bee96fe62f0e19
v.la <v.la@live.cn>
2020-04-01 15:30:50 +0800

    refactor: Improved packaging

--- build.cmd
+++ build.cmd
@@ -2,9 +2,10 @@ set artifacts=%~dp0artifacts
 
 if exist %artifacts%  rd /q /s %artifacts%
 
-call dotnet restore src/BloomFilter
-call dotnet restore src/BloomFilter.Redis
+set /p ver=<VERSION
 
+dotnet restore src/BloomFilter
+dotnet restore src/BloomFilter.Redis
 
-call dotnet pack src/BloomFilter -c release -o %artifacts%
-call dotnet pack src/BloomFilter.Redis -c release -o %artifacts%
+dotnet pack src/BloomFilter -c release  --version-suffix %ver% -o %artifacts%
+dotnet pack src/BloomFilter.Redis -c release  --version-suffix %ver% -o %artifacts%
