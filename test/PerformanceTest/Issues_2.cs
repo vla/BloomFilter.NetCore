458eae83bdfa7b4f68b273dc9413a1b30da80f69
v.la <v.la@live.cn>
2020-12-18 14:53:01 +0800

    test: Issues #2

---
+++ test/PerformanceTest/Issues_2.cs
@@ -0,0 +1,38 @@
+﻿using BloomFilter.Redis;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTest
+{
+    public class Issues_2
+    {
+        [Test("Redis Performance")]
+        public async Task Performance()
+        {
+            var bf = FilterRedisBuilder.Build<string>("localhost", "bftest", 100000, 0.001);
+
+            var items = new List<string>();
+
+            var count = 1000;
+
+            var rndNum = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < count; i++)
+            {
+                items.Add(Helper.GenerateString(rndNum.Next(5, 30)));
+            }
+
+            await bf.AddAsync(items);
+
+            var sw = Stopwatch.StartNew();
+
+            await bf.ContainsAsync(items);
+
+            Console.WriteLine(sw.Elapsed);//00:00:00.0959116
+        }
+    }
+}
