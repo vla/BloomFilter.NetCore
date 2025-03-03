22cd7d2c5bb139611a54a9357910f3c51c2973f9
v.la <v.la@live.cn>
2025-03-03 18:31:48 +0800

    test: Unit testing for Issues #14 #16

---
+++ test/BloomFilterTest/IssuesTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using BloomFilter;
+using BloomFilter.Configurations;
+using Xunit;
+
+namespace BloomFilterTest
+{
+    public class IssuesTest
+    {
+        [Theory]
+        [InlineData(100_000_000)]
+        [InlineData(1024 * 1024 * 1024)]
+        [InlineData(2147483640)]
+        [InlineData(2147483647)]
+        [InlineData(2147483641)]
+        [InlineData(2147483649)]
+        public void Issues_16(long expectedElements)
+        {
+            double errorRate = 0.0000001;
+            var hashMethod = HashMethod.XXHash128;
+
+            var bf = (FilterMemory)FilterBuilder.Build(expectedElements, errorRate, hashMethod);
+            var bytes = bf.ExportToBytes();
+
+            var bf2 = (FilterMemory)FilterBuilder.Build(new FilterMemoryOptions()
+            {
+                Method = hashMethod,
+                ExpectedElements = expectedElements,
+                ErrorRate = errorRate,
+                BucketBytes = bytes
+            });
+        }
+    }
+}
